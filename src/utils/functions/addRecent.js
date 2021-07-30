e4266e05e0c35aa825b71a06d5717feec7c7ee8a
cksdnr3 <cksdnr3333@naver.com>
2021-07-30 12:18:09 +0900

    Feat: utils/functions/addRecent.js 조회 이력 추가 기능

--- src/utils/functions/addRecent.js
+++ src/utils/functions/addRecent.js
@@ -1,3 +1,19 @@
-export default () => {
-    
-}
+const addRecent = recent => {
+  if (!window.localStorage.getItem('recents')) {
+    window.localStorage.setItem('recents', JSON.stringify([].concat(recent)));
+
+    return;
+  }
+  const recents = JSON.parse(window.localStorage.getItem('recents'));
+
+  //   if (recents.includes(v => v.id === recent.id)) {
+  //     updateRecent(new Date());
+  //   }
+
+  recents.push(recent);
+  window.localStorage.setItem('recents', JSON.stringify(recents));
+
+  return;
+};
+
+export default addRecent;
